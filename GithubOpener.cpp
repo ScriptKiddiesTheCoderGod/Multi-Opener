3e905085372f5b6f9d46a737a8220e3f404ca3b7
TrapLovers.cc <90288761+ScriptKiddiesTheCoderGod@users.noreply.github.com>
2021-09-09 02:00:11 +1000

    Add files via upload

---
+++ GithubOpener.cpp
@@ -0,0 +1,11 @@
+#include <Windows.h>
+
+///////////////////////////////
+ // #include <ThickAnimeGirl> // 
+///////////////////////////////
+
+
+int main()
+{
+    ShellExecute(0, 0, L"https://github.com", 0, 0, SW_SHOW);
+}
